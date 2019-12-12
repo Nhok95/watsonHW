7622c435137b2633630403028f07fc3a6860d978
Fihoca05 <Fihoca05@SINENSIA.EDU>
2019-12-12 15:33:22 +0100

    First commit

---
+++ src/main/java/com/example/watsonassistant/controllers/WatsonAssistantController.java
@@ -0,0 +1,17 @@
+package com.example.watsonassistant.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WatsonAssistantController {
+	
+	@RequestMapping(value="/send", method = RequestMethod.GET)
+	public String send(@RequestParam("message") String message ) {
+		
+		return message;
+	}
+
+}
---
+++ src/test/java/com/example/demospringbatch/WatsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
